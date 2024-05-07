81d3ef2220c176421ba520cdabe8801d6c3d715b
techiemow <mowriyaabk@gmail.com>
2024-05-07 17:36:02 +0530

    Booking Modal processing

--- src/components/HotelBookingModal.jsx
+++ src/components/HotelBookingModal.jsx
@@ -1,6 +1,10 @@
 import React from 'react';
 import { Grid, Modal, Typography,  } from '@mui/material'; // Import necessary components from Material-UI
 import { ModalDialog } from '@mui/joy';
+import ButtonGroup from '@mui/material/ButtonGroup';
+import Button from '@mui/material/Button';
+import AddIcon from '@mui/icons-material/Add';
+import RemoveIcon from '@mui/icons-material/Remove';
 import { DemoContainer, DemoItem } from "@mui/x-date-pickers/internals/demo";
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
@@ -56,8 +60,7 @@ const HotelBookingModal = ({ selectedHotelId, setselectedHotelId }) => {
             paddingBottom={8}
           >
             <Grid
-              item
-              style={{
+              item  style={{
                 width: "70%",
               }}
             >
@@ -69,20 +72,104 @@ const HotelBookingModal = ({ selectedHotelId, setselectedHotelId }) => {
                Check-IN Date
               </Typography>
               <LocalizationProvider dateAdapter={AdapterDayjs}>
-              <DemoContainer components={["DatePicker"]}>
+              <DemoItem components={["DatePicker"]}>
                   <DatePicker
                     views={["year", "month", "day"]}
                    
                     label="Select Date"
                     disablePast
                   />
-                </DemoContainer>
+                </DemoItem>
+                
+                
+                </LocalizationProvider>
+                </Grid>
+
+                <Grid
+              item  style={{
+                width: "70%",
+              }}
+            >
+              <Typography
+                textAlign={"center"}
+                id="nested-modal-title"
+                level="h5"
+              >
+               Check-OUT Date
+              </Typography>
+              <LocalizationProvider dateAdapter={AdapterDayjs}>
+              <DemoItem components={["DatePicker"]}>
+                  <DatePicker
+                    views={["year", "month", "day"]}
+                   
+                    label="Select Date"
+                    disablePast
+                  />
+                </DemoItem>
+                
+                
+                </LocalizationProvider>
+                </Grid>
+             
+
+
+                <Grid
+              item
+              style={{
+                width: "70%",
+              }}
+            >
+                 <Typography
+                textAlign={"center"}
+                id="nested-modal-title"
+                level="h5"
+              >
+                  Time
+              </Typography>
+        <LocalizationProvider dateAdapter={AdapterDayjs}>
                 <DemoItem componentName="TimePicker" valueType="time" >
           <TimePicker />
-        </DemoItem>
-                </LocalizationProvider>
+                 </DemoItem>
+        </LocalizationProvider>
+        </Grid>
+         <Grid item style={{width:"70%"}}>
+             <Typography
+                textAlign={"center"}
+                id="nested-modal-title"
+                level="h5"
+              >Rooms</Typography>
+
+       <ButtonGroup>
+          <Button
+            aria-label="reduce"
+            onClick={() => {
+              setCount(Math.max(count - 1, 0));
+            }}
+          >
+            <RemoveIcon fontSize="small" />
+          </Button>
+          <input type='Number'></input>
+          <Button
+            aria-label="increase"
+            onClick={() => {
+              setCount(count + 1);
+            }}
+          >
+            <AddIcon fontSize="small" />
+          </Button>
+        </ButtonGroup>
+
+            
+            
+            
+            
+            
+            
+            
+            </Grid>           
+                
            
-              </Grid>
+              
               </Grid>
 
       </ModalDialog>
